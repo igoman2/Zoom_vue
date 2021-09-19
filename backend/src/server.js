7a52b3199cd0c46b7ce7f6e741340d38bf1071c5
junho.han <junho.han@ebridge-world.com>
2021-09-19 17:47:17 +0900

    Feat: real time chatting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
-
 const app = express();
 // app.set("view engine", "pug");
 // app.set("views", __dirname + "/views");
@@ -14,10 +13,35 @@ const app = express();
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  Object.keys(rooms).map((key) => {
+    if (sids[key] === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms[roomName]?.length;
+}
+
 wsServer.on("connection", (socket) => {
+  console.log("connection");
+  socket["nickname"] = "Anon";
+  // socket.onAny((event) => {
+  //   console.log(`Socket Event: ${event}`);
+  // });
   socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("offer", (offer, roomName) => {
     socket.to(roomName).emit("offer", offer);
@@ -30,7 +54,22 @@ wsServer.on("connection", (socket) => {
   socket.on("ice", (ice, roomName) => {
     socket.to(roomName).emit("ice", ice);
   });
+
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+
+  socket.on("disconnecting", () => {
+    Object.keys(socket.rooms).map((room) => {
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
 });
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3001`);
+httpServer.listen(3001, handleListen);
