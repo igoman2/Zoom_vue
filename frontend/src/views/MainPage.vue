e9909831b0996615fe45982d65ac65180153bd07
junho.han <junho.han@ebridge-world.com>
2021-09-20 19:11:49 +0900

    action test

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container fluid cols="12" align="center" class="flex">
     <div class="text-center mb-16">
+      <div>
+        <!-- {{ hello }} -->
+      </div>
       <h1>JOOM</h1>
       <v-btn class="start-btn mt-16" color="primary" @click="goto('EnterRoom')">
         Get Started
@@ -10,13 +13,23 @@
 </template>
 
 <script>
+// import { getHello } from "@/service/room";
 export default {
   name: "MainPage",
   components: {},
-  data: () => ({}),
+  data: () => ({
+    hello: "",
+  }),
   computed: {},
-  created: function () {},
+  created: function () {
+    console.log("Hello!");
+    // this.init();
+  },
   methods: {
+    // async init() {
+    //   const resp = await getHello();
+    //   this.hello = resp;
+    // },
     goto(name) {
       this.$router.push({
         name: name,
