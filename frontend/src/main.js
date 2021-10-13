2370a8342804451c51e6007841de7e067edddf26
junho.han <junho.han@ebridge-world.com>
2021-10-13 16:41:47 +0900

    ec2 배포

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,10 @@ import "tiptap-vuetify/dist/main.css";
 import "vuetify/dist/vuetify.min.css";
 
 import io from "socket.io-client";
-const socket = io("http://localhost:3002");
+// const socket = io("http://localhost:3002");
+const socket = io(
+  "http://ec2-13-209-87-250.ap-northeast-2.compute.amazonaws.com:3002/"
+);
 Vue.prototype.$socket = socket;
 Vue.config.productionTip = false;
 
