9abfffa032843dcb21e46af0a01ad03b96f2de86
junho.han <junho.han@ebridge-workd.com>
2022-01-07 18:03:14 +0900

    deploy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,8 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueMeta from "vue-meta";
 
-// import plugin
 import { TiptapVuetifyPlugin } from "tiptap-vuetify";
-// don't forget to import CSS styles
 import "tiptap-vuetify/dist/main.css";
-// Vuetify's CSS styles
 import "vuetify/dist/vuetify.min.css";
 
 import io from "socket.io-client";
@@ -22,9 +19,7 @@ Vue.config.productionTip = false;
 
 // const vuetify = new Vuetify()
 Vue.use(TiptapVuetifyPlugin, {
-  // the next line is important! You need to provide the Vuetify Object to this place.
   vuetify,
-  // optional, default to 'md' (default vuetify icons before v2.0.0)
   iconsGroup: "mdi",
 });
 Vue.use(VueMeta);
